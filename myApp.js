203b0a9eef392e6abd23d2add1487975102f5add
somic <aoenax@gmail.com>
2021-11-20 13:36:14 +0600

    test

--- myApp.js
+++ myApp.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var app = express();
 
-
-
-
-app.get( '/', function( req, res ) {
+app.get('/', function(req, res) {
   let absolutePath = __dirname +
       '/views/index.html';
-  res.sendFile( absolutePath );
-} );
+  res.sendFile(absolutePath);
+});
 
 app.use('/public', express.static(__dirname + '/public'));
 
 module.exports = app;
 
+console.log('Hello World');
 
-console.log("Hello World");
+// EXERCISES FFC
+/*
+app.get('/json', function(req, res) {
+  let obj = {'message': 'Hello json'};
+  res.json(obj);
+});
+
+app.get('/', function(req, res) {
+  let absolutePath = __dirname +
+      '/views/index.html';
+  res.sendFile(absolutePath);
+});
+*/
